b7bf4f82bb231448e9e12699776bd8bf1afce973
AmirTallap <x-77@live.com>
2022-06-10 21:01:21 +0200

    Problem Number(0) Empty file.

---
+++ 0.php
@@ -0,0 +1,15 @@
+<?php
+//https://projecteuler.net/problem=0
+
+
+
+ini_set('display_errors', 1); 
+ini_set('display_startup_errors', 1); 
+error_reporting(E_ALL);
+function solution(){
+ 
+
+}
+echo solution();
+
+?>
